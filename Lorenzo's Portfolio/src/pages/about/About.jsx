f8a74f572a18fd7f2e45edfb9a96ad6776953fd3
LorenzoRiosWeb <100882991+LorenzoRiosWeb@users.noreply.github.com>
2024-05-20 18:21:14 -0600

    fixed some code

--- Lorenzo's Portfolio/src/pages/about/About.jsx
+++ Lorenzo's Portfolio/src/pages/about/About.jsx
@@ -6,6 +6,7 @@ const About = () => {
     <div className="about-container">
       <div className="about-section">
         <h2 className="about-heading">Hi, I'm Lorenzo Rios</h2>
+        <div className="about-line"></div>
         <p className="about-info">
           I am an aspiring web developer. Since the age of 14, I've always wondered how websites were made.
           Eventually, I let my curiosity guide me towards becoming a web developer. Although I faced challenges
@@ -14,6 +15,7 @@ const About = () => {
         </p>
       </div>
       <div className="goal-section">
+        <div className="goal-line"></div>
         <h2 className="goal-heading">My Goal</h2>
         <p className="goal-info">
           My goal is to empower new startups by crafting professional websites that enable them to expand their reach

7d8d61830b2914c482600f5fdc53798a7cf02a4f
LorenzoRiosWeb <100882991+LorenzoRiosWeb@users.noreply.github.com>
2024-05-20 21:37:06 -0600

    fixed some bugs

--- Lorenzo's Portfolio/vite.config.js
+++ Lorenzo's Portfolio/vite.config.js
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+      },
+    },
+  },
   server: {
     port: 3000,
-    open: true
-  }
-})
+    open: true,
+  },
+  base: './', // Ensures relative paths are used for assets
+});
